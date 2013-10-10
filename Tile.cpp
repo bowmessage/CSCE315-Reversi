0bfdb3bc9ae5b27f7f82f9cb4b8d11cc26e9a3f7
trogs <trogs@rocketmail.com>
2013-10-09 20:09:48 -0500

    Update Tile.cpp
    Added turnWhite, turnBlack, isWhite, isBlack, and isEmpty function definitions.

--- Tile.cpp
+++ Tile.cpp
@@ -5,3 +5,27 @@ Tile::Tile(){}
 Tile::Tile(State s){
   state = s;
 }
+
+
+Tile::turnWhite(){
+  state = Tile.WHITE;
+}
+
+Tile::turnBlack(){
+  state = Tile.BLACK;
+}
+
+Tile::isWhite(){
+  if(state == Tile.WHITE) return true;
+  else return false;
+}
+
+Tile::isBlack(){
+  if(state == Tile.BLACK) return true;
+  else return false;
+}
+
+Tile::isEmpty(){
+  if(state == Tile.BLANK) return true;
+  else return false;
+}
