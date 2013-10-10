693d3620ea53cab47b0e4cd5dea8b8ac8ee82463
James Bowman <jamesbowman@tamu.edu>
2013-10-10 18:51:28 -0500

    Added Server and basic socket behavior.

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,18 @@
-
+#include "Server.h"
 #include "Game.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-  Game g;
+/*  Game g;
   //cout << g.board;
   g.startGame();
   while(1){
     g.doTurn();
-  }
+  }*/
+
+  Server s;
+  s.startServer();
+
 }
