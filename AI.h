11ec621d916de0ba7ff18f7b384104c818a37567
Trevor James Gray <trogs@sun.cs.tamu.edu>
2013-10-16 16:52:58 -0500

    Updating AI.h

--- AI.h
+++ AI.h
@@ -1,1 +1,37 @@
+#include "Board.h"
 
+struct Node{
+	Node *right;
+	Node *left;
+	Move possible_move;
+
+	Node(Move move){
+		right = NULL;
+		left = NULL;
+		possible_move = move;
+	}
+	
+	void addRight(Node r){
+		right = r;
+	}
+	
+	void addLeft(Node l){
+		left = l;
+	}
+	
+	void removeLeft(){ left = NULL; }
+	
+	void removeRight(){ right = NULL; }
+
+}
+
+class AI{
+  public:
+	Node move_tree;
+	
+	AI();
+	~AI();
+	
+	updateMoveTree(Board gameboard);
+	
+}
